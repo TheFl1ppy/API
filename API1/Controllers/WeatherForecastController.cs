9c35730160a70b44af6df2e7e5022f0bf0220e4e
TheFl1ppy <111611313+TheFl1ppy@users.noreply.github.com>
2023-02-10 12:03:33 +0300

    Add files via upload

---
+++ API1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static List<string> Summaries = new()
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<string> Get()
+        {
+            return Summaries;   
+        }
+
+        [HttpPost]
+        public  IActionResult Add(string name)
+        {
+            if(name == null)
+            {
+                return BadRequest("Напиши какое-то имя");
+            }
+
+            Summaries.Add(name);
+            return Ok(Get());
+        }
+
+        [HttpPut]
+        public IActionResult Update(int index, string name)
+        {
+            if (index < 0 || index > Summaries.Count)
+            {
+                return BadRequest("Покажи мне такой элемент и я его изменю");
+            }
+
+            if (name == null)
+            {
+                return BadRequest("name не должно быть пустым");
+            }
+
+            Summaries[index] = name;
+            return Ok(Get());
+        }
+            
+        [HttpDelete]
+        public IActionResult Delete(int index)
+        {
+            if (index < 0 || index > Summaries.Count)
+            {
+                return BadRequest("Покажи мне такой элемент и я его удалю");
+            }
+
+            Summaries.RemoveAt(index);
+            return Ok(Get());
+        }
+
+        [HttpGet("{index}")]
+
+        public string Select(int index)
+        {
+            if (index < 0 || index > Summaries.Count)
+            {
+                return null;
+            }
+
+            string ans = Summaries[index];
+
+            return ans;
+        }
+    }
+}
